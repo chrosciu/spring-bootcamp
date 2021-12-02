2a302eefbb8b6bb19bd2bd51d530fcfe136c06aa
Marcin Chrost <marcinc@backbase.com>
2021-12-02 23:13:31 +0100

    More AOP examples

--- src/main/java/com/chrosciu/shop/payments/PaymentConsoleLogger.java
+++ src/main/java/com/chrosciu/shop/payments/PaymentConsoleLogger.java
@@ -1,16 +1,32 @@
 package com.chrosciu.shop.payments;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 @Slf4j
 public class PaymentConsoleLogger {
-    private static final String LOG_FORMAT = "A new payment of {} has been initiated";
+    @Before(value = "@annotation(LogPayments) && args(paymentRequest)")
+    public void beforePayment(PaymentRequest paymentRequest) {
+        log.info("New payment request: {}", paymentRequest);
+    }
+
+    @After("@annotation(LogPayments)")
+    public void afterPayment() {
+        log.info("After payment");
+    }
+
+    @AfterThrowing(value = "@annotation(LogPayments)", throwing = "exception")
+    public void onException(Exception exception) {
+        log.info("Payment exception: {}", exception.getClass().getSimpleName());
+    }
 
     @AfterReturning(value = "@annotation(LogPayments)", returning = "payment")
     public void log(Payment payment) {
-        log.info(LOG_FORMAT, payment);
+        log.info("A new payment of {} has been initiated", payment);
     }
 }
