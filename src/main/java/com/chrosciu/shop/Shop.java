179465ae1908ef783548f8f8e547310299fa783f
Marcin Chrost <marcinc@backbase.com>
2021-12-12 09:32:59 +0100

    dependency injection

--- src/main/java/com/chrosciu/shop/Shop.java
+++ src/main/java/com/chrosciu/shop/Shop.java
@@ -1,6 +1,8 @@
 package com.chrosciu.shop;
 
 import com.chrosciu.shop.payments.FakePaymentService;
+import com.chrosciu.shop.payments.IncrementalPaymentIdGenerator;
+import com.chrosciu.shop.payments.PaymentIdGenerator;
 import com.chrosciu.shop.payments.PaymentRequest;
 import com.chrosciu.shop.payments.PolishMoney;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Shop {
     public static void main(String[] args) {
-        var paymentService = new FakePaymentService();
+        PaymentIdGenerator paymentIdGenerator = new IncrementalPaymentIdGenerator();
+        var paymentService = new FakePaymentService(paymentIdGenerator);
         var paymentRequest = PaymentRequest.builder()
                 .money(PolishMoney.of(100))
                 .build();
