6a1c208e0f1844eb7b173803aefb8000101b5140
Marcin Chrost <marcinc@backbase.com>
2021-12-12 16:20:01 +0100

    integration tests

---
+++ src/test/java/com/chrosciu/shop/ShopTestConfig.java
@@ -0,0 +1,9 @@
+package com.chrosciu.shop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.chrosciu.shop")
+public class ShopTestConfig {
+}
