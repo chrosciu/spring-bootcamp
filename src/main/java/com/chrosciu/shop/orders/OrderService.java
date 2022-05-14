cb7c1a571169c371f0189ed749473c93c83081e1
Marcin Chrost <marcinc@backbase.com>
2022-05-14 07:39:31 +0200

    Order audit system

--- src/main/java/com/chrosciu/shop/orders/OrderService.java
+++ src/main/java/com/chrosciu/shop/orders/OrderService.java
@@ -1,13 +1,20 @@
 package com.chrosciu.shop.orders;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+
+import static com.chrosciu.shop.orders.OrderAuditEventType.CREATE;
+import static com.chrosciu.shop.orders.OrderAuditEventType.UPDATE;
 
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     public Order add(Order order) {
-        return orderRepository.save(order);
+        var savedOrder = orderRepository.save(order);
+        eventPublisher.publishEvent(new OrderAuditEvent(this, CREATE, savedOrder));
+        return savedOrder;
     }
 
     public Order getBy(Long orderId) {
@@ -16,6 +23,7 @@ public class OrderService {
     }
 
     public void update(Order order) {
+        eventPublisher.publishEvent(new OrderAuditEvent(this, UPDATE, order));
         orderRepository.update(order);
     }
 }
