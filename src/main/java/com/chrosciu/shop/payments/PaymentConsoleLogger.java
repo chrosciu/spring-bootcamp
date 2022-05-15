ef6345612026f928bc054972b5cad270131b5275
Marcin Chrost <marcinc@backbase.com>
2022-05-15 11:54:39 +0200

    java config

--- src/main/java/com/chrosciu/shop/payments/PaymentConsoleLogger.java
+++ src/main/java/com/chrosciu/shop/payments/PaymentConsoleLogger.java
@@ -3,9 +3,7 @@ package com.chrosciu.shop.payments;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
 @Aspect
 public class PaymentConsoleLogger {
--- src/main/java/com/chrosciu/shop/payments/PaymentsConfiguration.java
+++ src/main/java/com/chrosciu/shop/payments/PaymentsConfiguration.java
@@ -1,9 +1,34 @@
 package com.chrosciu.shop.payments;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class PaymentsConfiguration {
+    @Bean("paymentIdGenerator")
+    public PaymentIdGenerator incrementalPaymentIdGenerator() {
+        return new IncrementalPaymentIdGenerator();
+    }
+
+    @Bean
+    public PaymentIdGenerator uuidPaymentIdGenerator() {
+        return new UUIDPaymentIdGenerator();
+    }
+
+    @Bean
+    public PaymentRepository paymentRepository() {
+        return new HashMapPaymentRepository();
+    }
+
+    @Bean
+    public PaymentConsoleLogger paymentConsoleLogger() {
+        return new PaymentConsoleLogger();
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public PaymentService fakePaymentService(PaymentIdGenerator paymentIdGenerator, PaymentRepository paymentRepository) {
+        return new FakePaymentService(paymentIdGenerator, paymentRepository);
+    }
 }
