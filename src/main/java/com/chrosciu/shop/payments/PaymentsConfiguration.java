fbd46c10f10ac9806fb23d0c79b91a269e32b0d0
Marcin Chrost <marcinc@backbase.com>
2022-05-15 15:21:50 +0200

    async event listener

--- src/main/java/com/chrosciu/shop/payments/PaymentsConfiguration.java
+++ src/main/java/com/chrosciu/shop/payments/PaymentsConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAspectJAutoProxy
+@EnableAsync
 public class PaymentsConfiguration {
     @Bean("paymentIdGenerator")
     public PaymentIdGenerator incrementalPaymentIdGenerator() {
@@ -39,5 +43,13 @@ public class PaymentsConfiguration {
     @Bean
     public PaymentStatusChangeListener paymentStatusChangeListener() {
         return new PaymentStatusChangeListener();
+    }
+
+    @Bean
+    public TaskExecutor getAsyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(100);
+        executor.initialize();
+        return executor;
     }
 }
