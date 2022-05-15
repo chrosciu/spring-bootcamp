6cb4732c491ec738119b8db94c4ad26c22aa1a2c
Marcin Chrost <marcinc@backbase.com>
2022-05-15 11:32:47 +0200

    post construct

--- src/main/java/com/chrosciu/shop/payments/FakePaymentService.java
+++ src/main/java/com/chrosciu/shop/payments/FakePaymentService.java
@@ -1,22 +1,40 @@
 package com.chrosciu.shop.payments;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.time.Instant;
 
 @Slf4j
 @Service("paymentService")
 public class FakePaymentService implements PaymentService {
-    private final PaymentIdGenerator paymentIdGenerator;
-    private final PaymentRepository paymentRepository;
+    @Autowired
+    @Qualifier("uuid")
+    private PaymentIdGenerator paymentIdGenerator;
+    @Autowired
+    private PaymentRepository paymentRepository;
 
-    //@Autowired
-    public FakePaymentService(@Qualifier("uuid") PaymentIdGenerator paymentIdGenerator, PaymentRepository paymentRepository) {
-        this.paymentIdGenerator = paymentIdGenerator;
-        this.paymentRepository = paymentRepository;
+    public FakePaymentService() {
+        log.info("{}", paymentIdGenerator);
+        log.info("{}", paymentRepository);
     }
+
+    @PostConstruct
+    private void init() {
+        log.info("Payment service initialized");
+        log.info("{}", paymentIdGenerator);
+        log.info("{}", paymentRepository);
+    }
+
+    @PreDestroy
+    private void destroy() {
+        log.info("Payment service is going down");
+    }
+
 
     @Override
     @LogPayments
