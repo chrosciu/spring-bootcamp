b1f625ccdf461b06ed282da93899cc594785b2bc
Marcin Chrost <marcinc@backbase.com>
2022-05-15 11:46:20 +0200

    xml config

--- src/main/java/com/chrosciu/shop/payments/FakePaymentService.java
+++ src/main/java/com/chrosciu/shop/payments/FakePaymentService.java
@@ -2,16 +2,22 @@ package com.chrosciu.shop.payments;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
 public class FakePaymentService implements PaymentService {
     private final PaymentIdGenerator paymentIdGenerator;
     private final PaymentRepository paymentRepository;
+
+    private void init() {
+        log.info("Init");
+    }
+
+    private void destroy() {
+        log.info("Destroy");
+    }
 
     @Override
     @LogPayments
